ae2a183ba8ff90bd8b543ab27a5703233e6b4319
Arghdeep-hub <mallickarghadeep1@gmail.com>
2023-03-21 22:53:42 +0530

    final deployment

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyD01oMcpxmckXdAU92r2FTC0vY6-cUx95s",
+    authDomain: "repl-us.firebaseapp.com",
+    projectId: "repl-us",
+    storageBucket: "repl-us.appspot.com",
+    messagingSenderId: "153564218237",
+    appId: "1:153564218237:web:660b3908251fff28a24214",
+    measurementId: "G-M0DWS32XQW"
+});
+
+export default firebaseConfig
